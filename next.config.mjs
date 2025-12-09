d76f5a50a4ca233f6a2441b6dda14ee101bc2f8f
om9070 <om70796790@gmail.com>
2025-12-09 16:49:12 +0530

    update next02

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"], // Add allowed external domains here
   },
+    eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
